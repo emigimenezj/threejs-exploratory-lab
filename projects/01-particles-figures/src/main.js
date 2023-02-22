9425caa6b87aa9e19ea1f6224bf2b201809d9dea
Emilio Giménez <emigimenezj@zohomail.com>
2023-02-22 00:18:39 -0300

    Add render on screen

--- projects/01-particles-figures/src/main.js
+++ projects/01-particles-figures/src/main.js
@@ -33,7 +33,7 @@ let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10_000);
 
 camera.position.y = 25;
-camera.position.x = 36;
+camera.position.z = 36;
 
 let controls = new OrbitControls(camera);
 controls.update();
@@ -95,7 +95,128 @@ objLoader.load('Astronaut.obj', function(object){
       
       child.geometry.scale(scale, scale, scale);
       spacemanPoints = THREE.GeometryUtils.randomPointsInBufferGeometry(child.geometry, particleCount);
-      createVertices(spacemanParticles, spherePoints, yOffset, 3); 
+      createVertices(spacemanParticles, spacemanPoints, yOffset, 3); 
     }
   });
 });
+
+
+for (let p = 0; p < particleCount; p++) {
+  let vertex = new THREE.Vector3();
+  vertex.x = 0;
+  vertex.y = 0;
+  vertex.z = 0;
+
+  particles.vertices.push(vertex);
+}
+
+createVertices(sphereParticles, spherePoints, null, null);
+createVertices(cubeParticles, cubePoints, null, 1);
+
+function createVertices(emptyArray, points, yOffset = 0, trigger = null) {
+  for (let p = 0; p < particleCount; p++) {
+    let vertex = new THREE.Vector3();
+    vertex.x = points[p]['x'];
+    vertex.y = points[p]['y'] - yOffset;
+    vertex.z = points[p]['z'];
+
+    emptyArray.vertices.push(vertex);
+  }
+
+  if (trigger !== null) {
+    triggers[trigger].setAttribute('data-disabled', false);
+  }
+}
+
+let particleSystem = new THREE.PointCloud(
+  particles,
+  pMaterial
+);
+
+particleSystem.setParticles = true;
+
+scene.add(particleSystem);
+
+const normalSpeed = (defaultAnimationSpeed/100);
+let fullSpeed = (morphAnimationSpeed/100);
+
+let animationVars = {
+  speed: normalSpeed
+}
+
+function animate() {
+  stats.begin();
+  particleSystem.rotation.y += animationVars.speed;
+  particles.verticesNeedUpdate = true;
+  stats.end();
+
+  window.requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
+
+setTimeout(toSphere, 500);
+
+function toSphere() {
+  handleTriggers(0);
+  morphTo(sphereParticles);
+}
+
+function toCube() {
+  handleTriggers(1);
+  morphTo(cubeParticles);
+}
+
+function toRocket() {
+  handleTriggers(2);
+  morphTo(rocketParticles);
+}
+
+function toSpaceman() {
+  handleTriggers(3);
+  morphTo(spacemanParticles);
+}
+
+function morphTo(newParticles, color = '0xFFFFFF') {
+  TweenMax.to(animationVars, 0.3, {
+    ease: Power4.easeIn,
+    speed: fullSpeed,
+    onComplete: slowDown
+  });
+  particleSystem.material.color.setHex(color);
+
+  for (let i = 0; i < particles.vertices.length; i++) {
+    TweenMax.to(particles.vertices[i], 4, {
+      ease: Elastic.easeOut.config(1, 0.75),
+      x: newParticles.vertices[i].x,
+      y: newParticles.vertices[i].y,
+      z: newParticles.vertices[i].z
+    });
+  }
+}
+
+function slowDown() {
+  TweenMax.to(animationVars, 4, {
+    ease: Power2.easeOut,
+    speed: normalSpeed,
+    delay: 1
+  });
+}
+
+triggers[0].addEventListener('click', toSphere);
+triggers[1].addEventListener('click', toCube);
+triggers[2].addEventListener('click', toRocket);
+triggers[3].addEventListener('click', toSpaceman);
+
+function handleTriggers(disable) {
+  for (let x = 0; x < triggers.length; x++) {
+    if (disable === x) {
+      triggers[x].setAttribute('data-disabled', true);
+    } else {
+      triggers[x].setAttribute('data-disabled', false);
+    }
+  }
+}
+
+
